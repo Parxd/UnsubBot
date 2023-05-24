cef6b4af6aa098a8cd69799e34c56950ec4cc9ae
parxed <daniel.j.park@vanderbilt.edu>
2023-05-24 10:11:00 -0700

    add read email functionality

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import pickle
 import google.auth.credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
+import base64
+import email
+from bs4 import BeautifulSoup
 
 
 SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
@@ -29,14 +32,50 @@ def get_authenticated_service():
 
 def main():
     service = get_authenticated_service()
-    results = service.users().labels().list(userId='me').execute()
-    labels = results.get('labels', [])
-    if not labels:
-        print('No labels found.')
-    else:
-        print('Labels:')
-        for label in labels:
-            print(label['name'])
+    result = service.users().messages().list(userId='me').execute()
+  
+    # We can also pass maxResults to get any number of emails. Like this:
+    # result = service.users().messages().list(maxResults=200, userId='me').execute()
+    messages = result.get('messages')
+  
+    # messages is a list of dictionaries where each dictionary contains a message id.
+  
+    # iterate through all the messages
+    for msg in messages:
+        # Get the message from its id
+        txt = service.users().messages().get(userId='me', id=msg['id']).execute()
+
+        # Get value of 'payload' from dictionary 'txt'
+        payload = txt['payload']
+        headers = payload['headers']
+
+        # Look for Subject and Sender Email in the headers
+        for d in headers:
+            if d['name'] == 'Subject':
+                subject = d['value']
+            if d['name'] == 'From':
+                sender = d['value']
+
+        # The Body of the message is in Encrypted format. So, we have to decode it.
+        # Get the data and decode it with base 64 decoder.
+        if payload.get('parts') is not None:
+            parts = payload.get('parts')[0]
+            data = parts['body']['data']
+            data = data.replace("-","+").replace("_","/")
+            decoded_data = base64.b64decode(data)
+
+            # Now, the data obtained is in lxml. So, we will parse 
+            # it with BeautifulSoup library
+            soup = BeautifulSoup(decoded_data , "lxml")
+            body = soup.body()
+
+            # print("Subject: ", subject)
+            # print("From: ", sender)
+            # print("Message: ", body)
+            # print('\n')
+            for line in body:
+                if "unsubscribe" in line or "Unsubscribe" in line:
+                    print(line)
 
 
 if __name__ == "__main__":
